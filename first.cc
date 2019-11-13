e5b6168e746a0480e7dee9505c85abc5e6a02158
GouravReddy <53928121+GouravReddy@users.noreply.github.com>
2019-11-13 12:43:46 +0530

    Add files via upload

--- first.cc
+++ first.cc
@@ -19,6 +19,7 @@
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
+#include "ns3/flow-monitor-module.h"
 
 using namespace ns3;
 
@@ -27,8 +28,8 @@ NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 int
 main (int argc, char *argv[])
 {
-  //CommandLine cmd;
-  //cmd.Parse (argc, argv);
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
   
   Time::SetResolution (Time::NS);
   LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
@@ -37,67 +38,64 @@ main (int argc, char *argv[])
   NodeContainer nodes;
   nodes.Create (2);
 
-  NodeContainer nodes1;
-  nodes1.Create (2);
-
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-  PointToPointHelper pointToPoint1;
-  pointToPoint1.SetDeviceAttribute ("DataRate", StringValue ("7Mbps"));
-  pointToPoint1.SetChannelAttribute ("Delay", StringValue ("1ms"));
-
   NetDeviceContainer devices;
   devices = pointToPoint.Install (nodes);
 
-  NetDeviceContainer devices1;
-  devices1 = pointToPoint1.Install (nodes1);
-
-
   InternetStackHelper stack;
   stack.Install (nodes);
-  stack.Install (nodes1);
 
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4AddressHelper address1;
-  address1.SetBase ("10.1.2.0", "255.255.255.0");
 
   Ipv4InterfaceContainer interfaces = address.Assign (devices);
-  Ipv4InterfaceContainer interfaces1 = address1.Assign (devices1);
 
   UdpEchoServerHelper echoServer (9);
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
   serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
+  serverApps.Stop (Seconds (100.0));
 
   UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (50));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
   ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
   clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  clientApps.Stop (Seconds (100.0));
 
-  UdpEchoServerHelper echoServer1 (10);
+  
+  std::cout<<std::endl;
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+    // Now, do the actual simulation.
+    NS_LOG_INFO ("Run Simulation.");
+    Simulator::Stop (Seconds(100.0));
+    Simulator::Run ();
+    monitor->CheckForLostPackets ();
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier ( ));
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats ();
+    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); ++i)
+    {
+      Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
+      std::cout<<" t.sourceAddress = "<<t.sourceAddress<<", t.destinationAddress = "<<t.destinationAddress<<" Source port:"<<t.sourcePort<<" "<<"Destination port:"<<t.destinationPort<<"\n";
 
-  ApplicationContainer serverApps1 = echoServer1.Install (nodes1.Get (0));
-  serverApps1.Start (Seconds (1.0));
-  serverApps1.Stop (Seconds (10.0));
+        std::cout << "Flow " << i->first << " (" << t.sourceAddress << " -> " <<t.destinationAddress << ")\n";
+        std::cout << " Tx Bytes: " << i->second.txBytes << "\n";
+        std::cout << " Rx Bytes: " << i->second.rxBytes << "\n";
+        std::cout << " Tx Packets: " << i->second.txPackets << "\n";
+        std::cout << " Rx Packets: " << i->second.rxPackets << "\n";
+        std::cout << " Throughput: " << i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1000 / 1000 << "Mbps\n";
+    }
+    monitor->SerializeToXmlFile("lab-1.flowmon", true, true);
+    Simulator::Destroy ();
+    NS_LOG_INFO ("Done.");
+  
 
-  UdpEchoClientHelper echoClient1 (interfaces1.GetAddress (0), 10);
-  echoClient1.SetAttribute ("MaxPackets", UintegerValue (1));
-  echoClient1.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient1.SetAttribute ("PacketSize", UintegerValue (1024));
-
-  ApplicationContainer clientApps1 = echoClient1.Install (nodes1.Get (1));
-  clientApps1.Start (Seconds (2.0));
-  clientApps1.Stop (Seconds (10.0));
-
-  Simulator::Run ();
-  Simulator::Destroy ();
+  
   return 0;
 }
